cdc0eaa951c1969143860c6f852ec6da038c943f
FOURGOUX PAUL p0701968 <paul.fourgoux@etu.univ-lyon1.fr>
2020-03-11 17:47:00 +0100

    manage slider value change

--- iShop/FiltersViewController.swift
+++ iShop/FiltersViewController.swift
@@ -10,61 +10,25 @@ import UIKit
 
 class FiltersViewController: UITableViewController {
 
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var lblValue: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        let minValue: Float = 0.0
+        let maxValue: Float = 2500
+        let currentValue: Float = 1250
+        
+        slider.maximumValue = maxValue
+        slider.minimumValue = minValue
+        slider.value = currentValue
     }
-
-    // MARK: - Table view data source
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    // MARK: - Actions
+    
+    @IBAction func onValueChange(_ sender: UISlider) {
+        
+        lblValue.text = String(slider.value) + "€"
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
